786580d6dadca69868c0eb886e391a4043e5605b
emran71 <ekhan4475@gmail.com>
2024-05-21 15:14:17 +0600

    Zepto Task2 upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Zepto Apps</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="container">
+    <div class="centered-text">Zepto Apps</div>
+  </div>
+</body>
+</html>
